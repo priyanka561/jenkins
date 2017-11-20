df3e3353e486072108d7bb45864073734884f44f
EC2 Default User <ec2-user@ip-172-16-1-88.ec2.internal>
2017-11-20 09:00:10 +0000

    jenkins

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,7 @@
-#
 # Cookbook:: jenkins
 # Recipe:: default
-#
 # Copyright:: 2017, The Authors, All Rights Reserved.
+case node['platform_family']
+when 'amazon'
+  include_recipe 'jenkins::webpage'
+end
---
+++ recipes/webpage.rb
@@ -0,0 +1,12 @@
+# Cookbook Name:: jenkins
+# Recipe:: default
+# Copyright 2017, REANCLOUD
+package 'httpd' do
+  action :install
+end
+template '/var/www/html/index.html' do
+  source "index.html.erb"
+end
+service 'httpd' do
+  action [:enable , :start]
+end
